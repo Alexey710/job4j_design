b9e5fd8ddec9a27787bbc9910b0971dd1bd7a11d
Aleksey Krylov <elsite2015@yandex.ru>
2021-01-17 18:04:15 +0400

    2. Анализ доступности сервера. [#438562]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -4,32 +4,49 @@ import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Analizy {
-    private final List<String> interval = new LinkedList<>();
-    private final List<String> line = new LinkedList<>();
+    private final List<String> interval = new ArrayList<>();
 
     public void unavailable(String source, String target) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            bufferedReader.lines().forEach(line :: add);
-            int mark = 0;
-            for (String s: line) {
+            bufferedReader.lines().forEach(s -> {
                 String[] arr = s.split(" ");
-                if (arr[0].equals("500") && mark == 0 || arr[0].equals("400") && mark == 0) {
-                    interval.add(arr[1]);
-                    mark = 1;
+                if (arr[0].equals("400") || arr[0].equals("500")) {
+                    if (interval.size() > 0) {
+                        String before = interval.get(interval.size() - 1);
+                        if (!(before.equals("400") || before.equals("500"))) {
+                            interval.add(arr[1]);
+                            interval.add(arr[0]);
+                            interval.remove(interval.size() - 3);
+                        }
+                    } else {
+                        interval.add(arr[1]);
+                        interval.add(arr[0]);
+                    }
                 }
-                if (arr[0].equals("200") && mark == 1) {
-                    interval.add(arr[1]);
-                    mark = 0;
+                if (arr[0].equals("200") || arr[0].equals("300")) {
+                    if (interval.size() > 0) {
+                        String before = interval.get(interval.size() - 1);
+                        if (before.equals("400") || before.equals("500")) {
+                            interval.add(arr[1]);
+                            interval.add(arr[0]);
+                            interval.remove(interval.size() - 3);
+                        }
+                    }
                 }
-            }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        write(target);
+
+    }
+
+    private void write(String target) {
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             int count = 0;
             int lastSemicolon  = 0;
@@ -52,7 +69,6 @@ public class Analizy {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
 }
