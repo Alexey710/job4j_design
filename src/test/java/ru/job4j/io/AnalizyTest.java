3e5de3c7791459260fe70388d45898f6afb3964f
Aleksey Krylov <elsite2015@yandex.ru>
2021-01-18 10:30:30 +0400

    2. Анализ доступности сервера. [#438562]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,20 +1,37 @@
 package ru.job4j.io;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void unavailable() {
-        String source = "server_log.csv";
-        String target = "unavailable.csv";
-        new Analizy().unavailable(source, target);
+    public void testUsingTemporaryFolder() throws IOException {
+        File source = folder.newFile("server_log.csv");
+        File target = folder.newFile("unavailable.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01"
+                    + System.lineSeparator()
+                    + "500 10:57:01"
+                    + System.lineSeparator()
+                    + "400 10:58:01"
+                    + System.lineSeparator()
+                    + "200 10:59:01"
+                    + System.lineSeparator()
+                    + "500 11:01:02"
+                    + System.lineSeparator()
+                    + "200 11:02:02");
+        }
+        new Analizy().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         List<String> result = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(target))) {
             bufferedReader.lines().forEach(result :: add);
