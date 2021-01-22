1ece0a455e9bfc6b10582a414d8ead22c6bf3f60
Aleksey Krylov <elsite2015@yandex.ru>
2021-01-22 14:29:48 +0400

    5. Преобразование JSON в POJO. JsonObject [#438571]

---
+++ src/main/java/ru/job4j/io/gson/Country.java
@@ -0,0 +1,30 @@
+package ru.job4j.io.gson;
+
+public class Country {
+    private String country;
+    private int countryCode;
+
+    public Country() {
+    }
+
+    public Country(String country, int countryCode) {
+        this.country = country;
+        this.countryCode = countryCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{"
+                + "country='" + country + '\''
+                + ", countryCode=" + countryCode
+                + '}';
+    }
+}
--- src/main/java/ru/job4j/io/gson/Solder.java
+++ src/main/java/ru/job4j/io/gson/Solder.java
@@ -4,35 +4,36 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.Arrays;
 
-class Country {
-    private String country;
-    private int countryCode;
-
-    public Country(String country, int countryCode) {
-        this.country = country;
-        this.countryCode = countryCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Country{"
-                + "country='" + country + '\''
-                + ", countryCode=" + countryCode
-                + '}';
-    }
-}
-
 public class Solder {
     private  String rank;
     private  int age;
     private  Country country;
     private  String[] rewards;
 
+    public Solder() {
+    }
+
     public Solder(String rank, int age, Country country, String[] rewards) {
         this.rank = rank;
         this.age = age;
         this.country = country;
         this.rewards = rewards;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public String[] getRewards() {
+        return rewards;
     }
 
     @Override
