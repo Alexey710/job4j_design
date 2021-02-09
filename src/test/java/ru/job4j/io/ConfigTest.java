c02c0b7c3a4e8f477df16303bb623d76c8cf3a33
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-09 07:46:36 +0400

    maven test

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -12,8 +12,8 @@ public class ConfigTest {
         Config config = new Config(".\\app.properties");
         config.load();
 
-        assertThat(config.value("hibernate.connection.username"), is("postgres"));
-        assertThat(config.value("hibernate.connection.password"), is("password"));
+        assertThat(config.value("jdbc.connection.username"), is("postgres"));
+        assertThat(config.value("jdbc.connection.password"), is("postgres"));
         assertNull(config.value("comment"));
     }
 
