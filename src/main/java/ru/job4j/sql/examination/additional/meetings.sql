857f34ab8d7f6f65245b4aae822451909a835c3c
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-10 14:37:34 +0400

    1. Экзамен. Sql [#438495]

--- src/main/java/ru/job4j/sql/examination/additional/meetings.sql
+++ src/main/java/ru/job4j/sql/examination/additional/meetings.sql
@@ -12,20 +12,20 @@ user_id int references users(id), meetings_id int references meetings(id));
 insert into meetings(name) values ('meetings1'), ('meetings2'), ('meetings3');
 insert into users(name) values ('user1'), ('user2'), ('user3');
 insert into schedule(status, user_id, meetings_id)
-values ('TRUE', 1, 1), ('FALSE', 2, 1), ('TRUE', 1, 2);
---2. Нужно написать запрос, который получит список всех заявок и количество подтвердивших участников.
+values ('TRUE', 1, 1), ('FALSE', 2, 1), ('TRUE', 1, 2), ('TRUE', 2, 2);
+--2. Нужно написать запрос, название всех встреч и количество подтвердивших участников
 with schedule_users as
 (select s.status, s.meetings_id, u.name from schedule s full outer join users u on s.user_id=u.id)
-select (select count(status) from schedule where status = 'TRUE') as all_true,
-su.status, su.name, m.name from schedule_users su
+select
+ m.name, count(su.status) from schedule_users su
 full outer join meetings m on m.id=su.meetings_id
-where su.status is not null;
+ where  su.status = 'true' or su.status is null and m.name is not null group by  m.name, su.status;
 --3. Нужно получить все совещания, где не было ни одной заявки на посещения
 with schedule_users as
 (select s.status, s.meetings_id, u.name from schedule s full outer join users u on s.user_id=u.id)
 select m.name from schedule_users su
 full outer join meetings m on m.id=su.meetings_id
-where su.status is null;
+where su.status is null and m.name is not null or su.status = 'false';
 
 
 
