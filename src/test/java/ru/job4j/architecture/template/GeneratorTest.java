4306bb2c4dd41e04d70ddd3f14785d5a16f2f124
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-11 12:41:21 +0400

    3. Шаблонизатор. [#438582]

--- src/test/java/ru/job4j/architecture/template/GeneratorTest.java
+++ src/test/java/ru/job4j/architecture/template/GeneratorTest.java
@@ -1,23 +1,28 @@
 package ru.job4j.architecture.template;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GeneratorTest {
+    private final Generator generator = new Generator() {
+        @Override
+        public String produce(String template, Map<String, String> args) {
+            return null;
+        }
+    };
+    private static final Map<String, String> map = new HashMap<>();
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        map.put("name", "Alexey");
+        map.put("subject", "you");
+    }
 
     @Test
     public void produceExistingKey() {
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "Alexey");
-        map.put("subject", "you");
-        Generator generator = new Generator() {
-            @Override
-            public String produce(String template, Map<String, String> args) {
-                return null;
-            }
-        };
         String rsl = generator.produce("I am a ${name}, Who are ${subject}? ", map);
         String expected = String.format("I am a %s, Who are %s?", "Alexey", "you");
         Assert.assertEquals(rsl, expected);
@@ -26,29 +31,15 @@ public class GeneratorTest {
     @Test(expected = IllegalArgumentException.class)
     public void produceNotExistingKey() {
         Map<String, String> map = new HashMap<>();
-        map.put("name", "Alexey");
-        map.put("subject", "you");
-        Generator generator = new Generator() {
-            @Override
-            public String produce(String template, Map<String, String> args) {
-                return null;
-            }
-        };
         generator.produce("I am a ${surname}, Who are ${subject}? ", map);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void produceExcessKey() {
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "Alexey");
-        map.put("subject", "you");
-        map.put("something", "you");
-        Generator generator = new Generator() {
-            @Override
-            public String produce(String template, Map<String, String> args) {
-                return null;
-            }
-        };
-        generator.produce("I am a ${name}, Who are ${subject}? ", map);
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("name", "Alexey");
+        map1.put("subject", "you");
+        map1.put("something", "you");
+        generator.produce("I am a ${name}, Who are ${subject}? ", map1);
     }
 }
