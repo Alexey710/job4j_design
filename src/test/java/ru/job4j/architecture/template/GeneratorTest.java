6f393c5e65d7e50bb09b41b7a920c990cfd40921
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-11 12:35:51 +0400

    3. Шаблонизатор. [#438582]

---
+++ src/test/java/ru/job4j/architecture/template/GeneratorTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.architecture.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneratorTest {
+
+    @Test
+    public void produceExistingKey() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Alexey");
+        map.put("subject", "you");
+        Generator generator = new Generator() {
+            @Override
+            public String produce(String template, Map<String, String> args) {
+                return null;
+            }
+        };
+        String rsl = generator.produce("I am a ${name}, Who are ${subject}? ", map);
+        String expected = String.format("I am a %s, Who are %s?", "Alexey", "you");
+        Assert.assertEquals(rsl, expected);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceNotExistingKey() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Alexey");
+        map.put("subject", "you");
+        Generator generator = new Generator() {
+            @Override
+            public String produce(String template, Map<String, String> args) {
+                return null;
+            }
+        };
+        generator.produce("I am a ${surname}, Who are ${subject}? ", map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceExcessKey() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Alexey");
+        map.put("subject", "you");
+        map.put("something", "you");
+        Generator generator = new Generator() {
+            @Override
+            public String produce(String template, Map<String, String> args) {
+                return null;
+            }
+        };
+        generator.produce("I am a ${name}, Who are ${subject}? ", map);
+    }
+}
